5f7a42b634da9dd73bfe04840e18265128a4ff2c
Dave <davidmperich@gmail.com>
2023-02-13 14:38:17 -0700

    Initial Commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,20 @@
-import logo from './logo.svg';
+import React, { Component } from 'react';
+import Lottery from './Lottery';
 import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends Component {
+  constructor(props) {
+    super(props);
+    this.state = { num: 4 };
+  }
+  render() {
+    return (
+      <div className='App'>
+        <Lottery/>
+        <Lottery title='Mini' numBalls={4} maxNum={10}/>
+      </div>
+    );
+  }
 }
 
 export default App;
